52da67fb717de5015d40da18ecfaebc3171b914e
Robert Levchenko <ro.le.00@mail.ru>
2023-04-07 22:26:27 +0300

    db battle 6

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -70,10 +70,10 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'NAME': os.getenv('DB_NAME', default='github_actions'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
+        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
